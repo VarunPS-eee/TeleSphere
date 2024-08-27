75c9f34ae59e134c9993c29a8b0e2d26a5177a29
vikaskattamuri <vikaskattamuri7403@gmail.com>
2024-08-27 08:01:22 +0530

    Created the Swagger UI endpoint.

---
+++ src/main/java/com/telesphere/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.telesphere.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Telesphere API")
+                        .version("1.0")
+                        .description(
+                                "API documentation for Telesphere Application"));
+    }
+}
